fff254327f10b13ff635c0015b1cdcfa69d0d062
CK Huang <kertmanster@gmail.com>
2015-01-20 10:50:46 +0800

    Initial Commit

---
+++ LargestRect/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LargestRect
+//
+//  Created by CKHuang on 1/20/15.
+//  Copyright (c) 2015 CKHuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
